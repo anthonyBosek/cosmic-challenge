6ef878817c8b0d71e6081f2f4161c4351944b92c
Anthony Bosek <anthony.bosek@gmail.com>
2023-11-17 20:22:08 -0700

    relationships, associations, validations

--- server/debug.py
+++ server/debug.py
@@ -3,6 +3,8 @@
 from app import app
 from models import db, Planet, Scientist, Mission
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     with app.app_context():
-        import ipdb; ipdb.set_trace()
+        import ipdb
+
+        ipdb.set_trace()
